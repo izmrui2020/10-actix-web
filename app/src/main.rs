718eae91d21d07e0b31d5ff4c768605c2fef7819
rr-rr-re <rr.anonymous.2020@gmail.com>
2021-09-16 00:41:01 +0900

    add

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,19 +1,25 @@
-use actix_web::{get, App, HttpResponse, HttpServer};
+use actix_web::{App, HttpResponse, HttpServer, ResponseError, get};
+use thiserror::Error;
 
+#[derive(Error, Debug)]
+enum MyError {}
+impl ResponseError for MyError {
+    
+}
 
 #[get("/")]
-async fn index() -> Result<HttpResponse, actix_web::Error> {
+async fn index() -> Result<HttpResponse, MyError> {
     let response_body = "Hello world";
 
     Ok(HttpResponse::Ok().body(response_body))
 
 }
 
-#[actix-web::main]
+#[actix_web::main]
 async fn main() -> Result<(), actix_web::Error> {
     HttpServer::new(move || App::new().service(index))
         .bind("0.0.0.0:8080")?
         .run()
-        .await;
+        .await?;
     Ok(())
 }
